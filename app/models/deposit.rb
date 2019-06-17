3021d60b317fa53ca5e6522869fd04a3e64ebd9a
wanying <wanying@sinosunlabs.com>
2019-06-17 05:39:24 -0400

    fix deposit campaign logic

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -144,7 +144,7 @@ class Deposit < ActiveRecord::Base
   end
 
   def deposit_referral_campaign
-    referrer = Member.find_by(id: user.referral_id)
+    referrer = Member.find_by(id: member.referral_id)
     remark = "The person you referred, #{member.email} has made a deposit!"
     trigger_campaigns(member, ["%Deposit #{currency_id}%Referral%"].to_json, member.uid, remark) if referrer
   end
