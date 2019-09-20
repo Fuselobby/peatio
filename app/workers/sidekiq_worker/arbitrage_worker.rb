3c4962fd53a77cba80dc2b5e71860c28dcb77e00
Chris <christopher@fuselobby.com>
2019-09-20 16:17:58 +0800

    Fix api doc

--- app/workers/sidekiq_worker/arbitrage_worker.rb
+++
@@ -1,9 +0,0 @@
-module SidekiqWorker
-	class ArbitrageExecutorWorker
-	  include Sidekiq::Worker
-	  sidekiq_options retry: false
-	  def perform()
-	    puts "Performing arbitrage..."
-	  end
-	end
-end
